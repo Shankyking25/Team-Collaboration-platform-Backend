368d8917640dba934726614a68c679abdef46b6d
Shashank Singh <101120408+Shankyking25@users.noreply.github.com>
2025-08-15 02:04:45 +0530

    Delete Backend directory

--- Backend/validations/projectValidation.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const projectValidation = Joi.object({
-  name: Joi.string().required(),
-  description: Joi.string().allow('', null),
-});
-
-module.exports = projectValidation;
--- Backend/validations/teamValidation.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const teamValidation = Joi.object({
-  name: Joi.string().min(3).max(50).required(),
-  description: Joi.string().allow("").optional()
-});
-
-module.exports = teamValidation;
